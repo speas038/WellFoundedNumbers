7a85cff65a36ec81f0c76ddde9e97211259c6dd8
alex <as1881@txstate.edu>
2013-02-11 12:13:24 -0600

    added println but not working

--- src/foundedNumbers/FoundedNumbers.java
+++ src/foundedNumbers/FoundedNumbers.java
@@ -1,22 +1,37 @@
 package foundedNumbers;
 
+import java.io.*;
+
 public class FoundedNumbers {
 	
 	public static void main(String[] args){
 		
 		String s = "0";
 		
-		for (int i = 1; i<=7; i++){
-			for (int j = i+1; j <= 9; j++){
-				for (int k = j+1; k<= 9; k++){
-					for (int l = k+1; l<=9; l++){
-						s = String.format("%d%d%d%d",i,j,k,l);
-						System.out.println(s);
+		try {
+		PrintStream	fout = new PrintStream(new FileOutputStream("output.txt"));
+			
+			for (int i = 1; i<=7; i++){
+				for (int j = i+1; j <= 9; j++){
+					for (int k = j+1; k<= 9; k++){
+						for (int l = k+1; l<=9; l++){
+						
+							s = String.format("%d%d%d%d",i,j,k,l);
+							fout.println(s);
+							System.out.println(s);
+							
+						}
 					}
 				}
 			}
+			fout.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+
 	}
 
 }
